956b78064085869fa45f6a83b83863813c1bbe3f
Sean Coley <seanwcoley@gmail.com>
2020-07-01 16:55:53 +0200

    Initial commit (some edits to config)

---
+++ config.js
@@ -0,0 +1,57 @@
+"use strict";
+
+module.exports = {
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Random Neural Firing",
+  subtitle: `A cross-section of who I am and what I can do with a little bit of what I've done and what I'm doing.`,
+  copyright: "All content published under CC BY-NC-SA 4.0 license",
+  disqusShortname: "",
+  postsPerPage: 4,
+  googleAnalyticsId: "",
+  useKatex: false,
+  menu: [
+    {
+      label: "Me",
+      path: "/pages/me",
+    },
+    {
+      label: "What I do",
+      path: "/pages/do",
+    },
+    {
+      label: `What I've built`,
+      path: "/pages/build",
+    },
+    {
+      label: `What I've written`,
+      path: "/pages/write",
+    },
+    {
+      label: `Contact`,
+      path: "/pages/contact",
+    },
+  ],
+  author: {
+    name: "Sean Coley",
+    photo: "/photo.jpg",
+    bio: "",
+    contacts: {
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+    },
+  },
+};
